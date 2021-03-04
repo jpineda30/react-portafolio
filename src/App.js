6943685cbdc82f998f1e3861a74539c58dfb3790
pepe30 <jose.pinedaesquer@gmail.com>
2021-03-03 19:20:24 -0600

    added menu

--- src/App.js
+++ src/App.js
@@ -7,12 +7,32 @@ function App() {
     <div className="container flex-col">
 
       <div className="menus flex-row flex-wrap">
-        <div className="main-button"></div>
-        <div className="main-button"></div>
-        <div className="main-button"></div>
-        <div className="main-button"></div>
-        <div className="main-button"></div>
-        <div className="main-button"></div>
+
+        <div className="main-button s1">
+          <i className="fas fa-house-user"></i>
+          <h1>HOME</h1>
+        </div>
+
+        <div className="main-button s2">
+        <i className="fas fa-suitcase"></i>
+          <h1>PORTAFOLIO</h1>
+        </div>
+        <div className="main-button s3">
+        <i className="far fa-file-alt"></i>
+          <h1>RESUME</h1>
+        </div>
+        <div className="main-button s4">
+        <i className="fab fa-linkedin"></i>
+          <h1>LINKEDIN</h1>
+        </div>
+        <div className="main-button s5">
+        <i className="fab fa-github"></i>
+          <h1>GITHUB</h1>
+        </div>
+        <div className="main-button s6">
+        <i className="far fa-address-book"></i>
+          <h1>CONTACT</h1>
+        </div>
 
       </div>
      
