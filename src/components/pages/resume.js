e6753ff28247c635f36518ea4239f7787a2372d6
pepe30 <jose.pinedaesquer@gmail.com>
2021-05-07 16:51:01 -0500

    added syles

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,25 +1,12 @@
 import React from "react";
 import MyResume from "../myResume";
 
-
-
 function Resume() {
-
-return(
-
-        
-        <div className="s1b contents">
-            
-            <MyResume/>
-            
-           
-           
-
-        </div>    
-
-
-
-    );
+  return (
+    <div className="contents">
+      <MyResume />
+    </div>
+  );
 }
 
 export default Resume;
